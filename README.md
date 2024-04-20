cd4ef7e6377a859ed2fe22aff4a69214589ec515
Plamedi Luis <plamedi.luis11@gmail.com>
2024-04-20 19:55:15 +0200

    created methods

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Li8Z
 
-Hey, this is **Li8Z**, an attempt at implementing a Chip-8 emulator in Ziglang.
+Hey, this is **Li8Z**, an attempt at implementing a Chip-8 emulator in Zig.
 I was inspired to try this project thanks to my
-[buddy](https://github.com/kevin-nel).  The project will be heavily guided by
-this beautiful [guide](https://github.com/aquova/chip8-book).  When I feel the
+[buddy](https://github.com/kevin-nel). The project will be heavily guided by
+this beautiful [guide](https://github.com/aquova/chip8-book). When I feel the
 project is complete I will tag a release, until then thank you for reading and
 hope you are well.
--- src/backend.zig
+++ src/backend.zig
@@ -5,6 +5,25 @@ const RAM_SIZE: usize = 4096;
 const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
+const FONTSET_SIZE: usize = 80;
+const FONTSET = [FONTSET_SIZE]u8{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+};
 
 pub const Emu = struct {
     const START_ADDR: u16 = 0x200;
@@ -20,7 +39,7 @@ pub const Emu = struct {
     st: u8,
 
     pub fn init() Emu {
-        return Emu{
+        var new_emu = Emu{
             .pc = START_ADDR,
             .ram = undefined,
             .screen = undefined,
@@ -32,19 +51,72 @@ pub const Emu = struct {
             .dt = 0,
             .st = 0,
         };
+
+        const ram_slice = new_emu.ram[0..80];
+        ram_slice.* = FONTSET;
+        return new_emu;
+    }
+
+    fn push(self: *Emu, val: u16) void {
+        self.stack[@intCast(self.sp)] = val;
+        self.sp += 1;
+    }
+
+    fn pop(self: *Emu) u16 {
+        self.sp -= 1;
+        return self.stack[@intCast(self.sp)];
+    }
+
+    pub fn reset(self: *Emu) void {
+        self.pc = Emu.START_ADDR;
+        self.i_reg = 0;
+        self.sp = 0;
+        self.dt = 0;
+        self.st = 0;
+        self.screen = undefined;
+        self.v_reg = undefined;
+        self.stack = undefined;
+        self.keys = undefined;
+        self.ram = undefined;
+        const ram_slice = self.ram[0..80];
+        ram_slice.* = FONTSET;
+    }
+
+    pub fn tick(self: *Emu) void {
+        const op = self.fetch();
+        self.execute(op);
+    }
+
+    fn execute(self: *Emu, op: u16) void {
+        // TODO
+    }
+
+    fn fetch(self: *Emu) u16 {
+        const higher_byte: u16 = self.ram[@intCast(self.pc)];
+        const lower_byte: u16 = self.ram[@intCast(self.pc + 1)];
+        const op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        return op;
+    }
+
+    pub fn tick_timers(self: *Emu) void {
+        if (self.dt > 0) self.dt -= 1;
+
+        if (self.st > 0) {
+            if (self.st == 1) {
+                // GONNA BEEP HERE
+            }
+            self.st -= 1;
+        }
     }
 };
 
-test "Default arrays correct" {
+test "Undefined arrays correct" {
     const std = @import("std");
 
     const emu = Emu.init();
 
     try std.testing.expectEqual(512, emu.pc);
-
-    for (emu.ram) |i| {
-        try std.testing.expectEqual(0, i);
-    }
 
     for (emu.screen) |i| {
         try std.testing.expectEqual(false, i);
@@ -61,4 +133,12 @@ test "Default arrays correct" {
     for (emu.keys) |i| {
         try std.testing.expectEqual(false, i);
     }
+}
+
+test "FONTSET slice write" {
+    const std = @import("std");
+
+    const emu = Emu.init();
+
+    try std.testing.expectEqual(FONTSET, emu.ram[0..80].*);
 }
