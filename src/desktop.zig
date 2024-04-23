141a407308bada5efd6a54f870a5091892be2986
Plamedi Luis <plamedi.luis11@gmail.com>
2024-04-23 22:14:19 +0200

    placeholder controls before better solution

--- src/desktop.zig
+++ src/desktop.zig
@@ -8,6 +8,47 @@ const WINDOW_WIDTH: u32 = li.SCREEN_WIDTH * SCALE;
 const WINDOW_HEIGHT: u32 = li.SCREEN_HEIGHT * SCALE;
 const TICKS_PER_FRAME: usize = 10;
 
+const game_keys = [_]i32{
+    ray.KEY_ONE,
+    ray.KEY_TWO,
+    ray.KEY_THREE,
+    ray.KEY_FOUR,
+    ray.KEY_Q,
+    ray.KEY_W,
+    ray.KEY_E,
+    ray.KEY_R,
+    ray.KEY_A,
+    ray.KEY_S,
+    ray.KEY_D,
+    ray.KEY_F,
+    ray.KEY_Z,
+    ray.KEY_X,
+    ray.KEY_C,
+    ray.KEY_V,
+};
+
+fn key2btn(key: c_int) ?usize {
+    switch (key) {
+        ray.KEY_ONE => return 0,
+        ray.KEY_TWO => return 1,
+        ray.KEY_THREE => return 2,
+        ray.KEY_FOUR => return 3,
+        ray.KEY_Q => return 4,
+        ray.KEY_W => return 5,
+        ray.KEY_E => return 6,
+        ray.KEY_R => return 7,
+        ray.KEY_A => return 8,
+        ray.KEY_S => return 9,
+        ray.KEY_D => return 10,
+        ray.KEY_F => return 11,
+        ray.KEY_Z => return 12,
+        ray.KEY_X => return 13,
+        ray.KEY_C => return 14,
+        ray.KEY_V => return 15,
+        else => return null,
+    }
+}
+
 fn drawScreen(emu: *li.Emu) void {
     const screen_buf = emu.getDisplay();
 
@@ -62,6 +103,36 @@ pub fn main() !void {
     while (!ray.WindowShouldClose()) {
         ray.BeginDrawing();
         defer ray.EndDrawing();
+
+        for (game_keys) |key| {
+            if (ray.IsKeyDown(key)) {
+                if (key2btn(key)) |k| {
+                    chip.keypress(k, true);
+                }
+            }
+
+            if (ray.IsKeyUp(key)) {
+                if (key2btn(key)) |k| {
+                    chip.keypress(k, false);
+                }
+            }
+        }
+
+        // if (ray.IsKeyPressed(ray.KEY_ESCAPE)) {
+        //     break;
+        // }
+        //
+        // if (ray.IsKeyDown(ray.GetKeyPressed())) {
+        //     if (key2btn(ray.GetKeyPressed())) |k| {
+        //         chip.keypress(k, true);
+        //     }
+        // }
+        //
+        // if (ray.IsKeyUp(ray.GetKeyPressed())) {
+        //     if (key2btn(ray.GetKeyPressed())) |k| {
+        //         chip.keypress(k, false);
+        //     }
+        // }
 
         for (0..TICKS_PER_FRAME) |_| {
             chip.tick();
