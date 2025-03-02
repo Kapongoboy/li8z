fc843c3b2e277c836922e714820c876c8656826a
Plamedi Luis <plamedi.luis11@gmail.com>
2025-03-02 18:09:51 +0200

    fix(bootstrap): switch to cmake build commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ cd .packages
 git clone --depth 1 https://github.com/raysan5/raylib.git
 git clone --depth 1 https://github.com/zig-gamedev/zaudio.git
 cd raylib
-zig build --release=fast
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build build --parallel 4
 cd ../..
