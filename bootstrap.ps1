88607d306ad0413ebab8ff1463e7ff6b8c220bba
Plamedi on Arch <plamedi.luis11@gmail.com>
2025-03-05 10:11:17 +0200

    chore: update bootstrap scripts with additional dependencies

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,10 @@
-New-Item -Path ".\packages" -ItemType Directory -Force
-Set-Location ".\packages"
+New-Item -Path ".packages" -ItemType Directory -Force
+Set-Location ".packages"
 git clone --depth 1 https://github.com/raysan5/raylib.git
-git clone --depth 1 https://github.com/zig-gamedev/zig-gamedev.git
-Set-Location ".\raylib"
-zig build --release=fast
+git clone --depth 1 https://github.com/zig-gamedev/zaudio.git
+git clone --depth 1 http://git.code.sf.net/p/tinyfiledialogs/code tinyfiledialogs
+git clone --depth 1 https://github.com/raysan5/raygui.git
+Set-Location "raylib"
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build build --parallel 4
 Set-Location "../.."
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@ mkdir .packages
 cd .packages
 git clone --depth 1 https://github.com/raysan5/raylib.git
 git clone --depth 1 https://github.com/zig-gamedev/zaudio.git
+git clone --depth 1 http://git.code.sf.net/p/tinyfiledialogs/code tinyfiledialogs
+git clone --depth 1 https://github.com/raysan5/raygui.git
 cd raylib
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build build --parallel 4
