123f378a1aac5aa642c375d820c4360d63b594eb
Plamedi on Arch <plamedi.luis11@gmail.com>
2025-03-05 10:03:33 +0200

    chore: update build script run step name for desktop app

--- build.zig
+++ build.zig
@@ -48,7 +48,7 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("desktop", "build and run the desktop app");
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
